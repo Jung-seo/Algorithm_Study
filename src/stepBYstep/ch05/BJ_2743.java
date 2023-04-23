d116bd2e5ebf268fd48f57ac507b87c524e25f69
jungseo <jungwook22@gmail.com>
2023-04-23 23:34:41 +0900

    ch05 덜품

---
+++ src/stepBYstep/ch05/BJ_2743.java
@@ -0,0 +1,11 @@
+package stepBYstep.ch05;
+
+import java.util.Scanner;
+
+public class BJ_2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().length());
+
+    }
+}
