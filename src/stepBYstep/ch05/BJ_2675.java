4de723e55d9ee2f0db8e46bc2f8039bffaf76349
jungseo <jungwook22@gmail.com>
2023-04-25 16:18:05 +0900

    clear

--- src/stepBYstep/ch05/BJ_2675.java
+++ src/stepBYstep/ch05/BJ_2675.java
@@ -9,21 +9,21 @@ public class BJ_2675 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder("");
 
         int testcase = sc.nextInt();
-        sc.nextLine();
 
         for (int i = 0; i < testcase; i++) {
             int num = sc.nextInt();
-            sc.nextLine();
-
+            // 여기서 공백이 생기는데
+            // sc.nextLine() 넣으면 정상 작동을 안해...
             String str = sc.nextLine();
 
             for (int j = 0; j < str.length(); j++) {
-               sb.append(str.substring(j).repeat(num - 1));
+                sb.append(Character.toString(str.charAt(j)).repeat(num));
             }
+            sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString().replace(" ", ""));
     }
 }
