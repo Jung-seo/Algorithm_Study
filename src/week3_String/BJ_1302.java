441175df6b1fb0f7106775ec229dc8135477e82d
Jung-seo <jungwook22@gmail.com>
2023-05-02 22:31:58 +0900

    외않되

--- src/week3_String/BJ_1302.java
+++ src/week3_String/BJ_1302.java
@@ -1,2 +1,46 @@
-package week3_String;public class BJ_1302 {
+package week3_String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class BJ_1302 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cases = Integer.parseInt(br.readLine());
+
+        String[] bookName = new String[cases];
+
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        int count = 0;
+
+        for (int i = 0; i < cases; i++) {
+            bookName[i] = br.readLine();
+        }
+//        배열에 책 이름 담고
+        for (int i = 0; i < cases; i ++) {
+            for (int j = 0; j < cases; j++) {
+                if (bookName[i].equals(bookName[j])) {
+                    count++;
+                }
+            }
+//            트리맵에 <책이름, 수량> 넣고
+            map.put(bookName[i], count);
+            count = 0;
+        }
+        int max = 0;
+        String needThisBook = "";
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
+                needThisBook = entry.getKey();
+                max = entry.getValue();
+            }
+        }
+        System.out.println(needThisBook);
+    }
 }
--- src/week3_String/BJ_5525.java
+++ src/week3_String/BJ_5525.java
@@ -1,2 +1,24 @@
-package week3_String;public class BJ_5525 {
+package week3_String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_5525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numO = Integer.parseInt(br.readLine());
+        br.readLine();
+        String str = br.readLine();
+
+        String[] arr = str.split("I");
+
+        int count = 0;
+//i + N - 1 까지 반복되는 O의 횟수.. 구현을...
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].equals("O") && arr[i + 1].equals("O")) count++;
+        }
+        System.out.println(count);
+    }
 }
