ef395afda0085483d5fb91cf523a1509514dc87d
Jung-seo <jungwook22@gmail.com>
2023-05-02 18:54:18 +0900

    5635 clear

--- src/week3_String/BJ_5635.java
+++ src/week3_String/BJ_5635.java
@@ -1,30 +1,56 @@
 package week3_String;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.*;
+
 
 public class BJ_5635 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         int people = Integer.parseInt(sc.nextLine());
-        String[][] arr = new String[people][4];
+        HashMap<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < people; i++) {
-            String str = sc.nextLine();
-            StringTokenizer st = new StringTokenizer(str);
-            for (int j = 0; j < 4; j++) {
-                arr[i][j] = st.nextToken();
+            String name = sc.next();
+
+            String birth = sc.nextLine();
+            String[] arr = birth.split(" ");
+
+            for (int j = 0; j < 3; j++) {
+                arr[j].replace(" ", "");
+                if (arr[j].length() == 1) arr[j] = "0" + arr[j]; // 1자리 숫자면 앞에 0 추가
+            }
+            //연 월 일 순으로 만들고
+            String temp = arr[1];
+            arr[1] = arr[3];
+            arr[3] = temp;
+
+            String str = Arrays.toString(arr).replace(" ", "").replace("[", "").replace("]", "").replace(",", "");
+            int intBirth = Integer.parseInt(str);
+
+            map.put(name, intBirth);
+        }
+
+        int max = 19891231;
+        String youngest = "";
+        int min = 20110101;
+        String oldest = "";
+
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() < min) {
+                min = entry.getValue();
+                oldest = entry.getKey();
+            }
+            if (entry.getValue() > max){
+                max = entry.getValue();
+                youngest = entry.getKey();
             }
         }
 
-        int oldestYear = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (Integer.parseInt(arr[i][3]) > oldestYear) {
-                oldestYear = Integer.parseInt(arr[i][3]);
-            }
-
-        }
+        System.out.println(youngest);
+        System.out.println(oldest);
     }
 }
