213cdabc9541af3fbe1635c0c40d81f0fa0d9544
jungseo <jungwook22@gmail.com>
2023-05-10 22:32:40 +0900

    스텍1, 재귀1 완

--- src/week4_Stack/BJ_17413.java
+++ src/week4_Stack/BJ_17413.java
@@ -4,19 +4,43 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class BJ_17413 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         Stack<Character> stack = new Stack<>();
 
         String str = br.readLine();
 
-        String[] arr = str.split("<");
-
-        for (int i = 0; i < arr.length; i ++) {
-            if (arr[i].endsWith(">"))
+        for (int i = 0; i < str.length(); i++) {
+//            < 나올때
+            if (str.charAt(i) == '<') {
+                while (str.charAt(i) != '>') {
+                    sb.append(str.charAt(i));
+                    i++;
+                }
+                i--;
+            }
+            else if (str.charAt(i) == ' '){
+                sb.append(" ");
+            }
+            else if (str.charAt(i) == '>'){
+                sb.append(">");
+            }
+//            나머지
+            else {
+                while (str.charAt(i) != ' ' && str.charAt(i) != '<') {
+                    stack.push(str.charAt(i));
+                    i++;
+                    if (i == str.length()) break;
+                }
+                i--;
+                while (!stack.isEmpty())
+                    sb.append(stack.pop());
+            }
         }
+        System.out.println(sb);
     }
 }
+
---
+++ src/week4_Stack/BJ_3986.java
@@ -0,0 +1,14 @@
+package week4_Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_3986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+    }
+}
