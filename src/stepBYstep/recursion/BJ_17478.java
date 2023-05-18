5cb7e1721e18cc8ff7f758ead5292c3919344f19
jungseo <jungwook22@gmail.com>
2023-05-18 19:44:17 +0900

    재귀함수는 뭔가요? 17478 해결

--- src/stepBYstep/recursion/BJ_17478.java
+++ src/stepBYstep/recursion/BJ_17478.java
@@ -10,34 +10,26 @@ public class BJ_17478 {
         int i = 0;
 
         System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-        recurse(num);
-        System.out.println("\"재귀함수가 뭔가요?\"");
-        System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-        recurse2(num);
-        System.out.println("라고 답변하였지.");
+        recurse(0, num);
     }
-    static void recurse(int num){
-        num--;
-        if (num < 0) return;
-        recurse3(num, 0);
-        System.out.println("\"재귀함수가 뭔가요?\"");
-        System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+    static void recurse(int depth, int num){
+        String under = "";
+        for (int i = 0; i < depth; i++) {
+            under += "____";
+        }
+        if (depth == num) {
+            System.out.println(under + "\"재귀함수가 뭔가요?\"");
+            System.out.println(under + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+            System.out.println(under + "라고 답변하였지.");
+            return;
+        }
 
-        recurse(num);
-    }
-    static void recurse2(int num) {
-        num--;
-        if (num < 0) return;
+        System.out.println(under + "\"재귀함수가 뭔가요?\"");
+        System.out.println(under + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+        System.out.println(under + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+        System.out.println(under + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
 
-        System.out.println("라고 답변하였지.");
-        recurse2(num);
-    }
-    static void recurse3(int num, int i) {
-        i++;
-        if (i > num) return;
-
-        System.out.print("_".repeat(4 * i));
+        recurse(depth + 1, num);
+        System.out.println(under + "라고 답변하였지.");
     }
 }
