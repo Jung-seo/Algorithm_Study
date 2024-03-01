3df058398762f2086a9e17a16302bae5d5a0fc44
jungseo <jungwook22@gmail.com>
2024-03-01 19:45:36 +0900

    BJ 1620

--- src/study/hash/PocketmonMaster.java
+++ src/study/hash/PocketmonMaster.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 
 //https://www.acmicpc.net/problem/1620
 public class PocketmonMaster {
@@ -15,39 +14,25 @@ public class PocketmonMaster {
         String[] num = br.readLine().split(" ");
         int n = Integer.parseInt(num[0]);
         int m = Integer.parseInt(num[1]);
-        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<Integer, String> numName = new HashMap<>();
+        HashMap<String, Integer> nameNum = new HashMap<>();
 
         for (int i = 1; i <= n; i++) {
-            map.put(i, br.readLine());
+            String str = br.readLine();
+            numName.put(i, str);
+            nameNum.put(str, i);
         }
 
         for (int i = 0; i < m; i++) {
             String input = br.readLine();
 
-            if (!map.containsValue(input)) {
-                sb.append(map.get(Integer.parseInt(input))).append("\n");
+            if (nameNum.containsKey(input)) {
+                sb.append(nameNum.get(input)).append("\n");
             } else {
-                sb.append(getKey(map, input)).append("\n");
+                sb.append(numName.get(Integer.parseInt(input))).append("\n");
             }
         }
 
         System.out.print(sb.toString().trim());
-    }
-
-    static Integer getKey(HashMap<Integer, String> map, String name) {
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            if (entry.getValue().equals(name)) {
-                return entry.getKey();
-            }
-        }
-        return null;
-    }
-
-    static Integer getKey1(HashMap<Integer, String> map, String name) {
-        return map.entrySet().stream()
-                .filter(e -> e.getValue().equals(name))
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(null);
     }
 }
